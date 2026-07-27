c69ce72db998edccf79066db62f7f0b10eb8daba
xiaoshukeji <wanghanduo2021@162.com>
2026-07-28 01:53:41 +0800

    Site updated: 2026-07-28 01:53:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/28/hello-world/"];function toRandomPost(){
+var posts=["2026/07/28/hello-world/","2026/07/28/ai-agent-notes/","2026/07/28/frontend-roadmap/","2026/07/28/life-bright/","2026/07/28/project-build-log/","2026/07/28/year-summary/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
